6015b803581b9f42fd70efe05472474f30e38bab
RedBiscuits <84057046+RedBiscuits@users.noreply.github.com>
2022-08-12 22:24:02 +0200

    instructions ViewModel step 10/15

---
+++ starter/app/src/main/java/com/udacity/shoestore/ViewModel.kt
@@ -0,0 +1,9 @@
+package com.udacity.shoestore
+
+import androidx.lifecycle.MutableLiveData
+import com.udacity.shoestore.models.Shoe
+
+class ViewModel(){
+    var mutableLiveData:MutableLiveData<ArrayList<Shoe>> = MutableLiveData()
+
+}
